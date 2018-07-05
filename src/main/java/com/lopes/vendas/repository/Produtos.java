a8258f95a19f6e4fcd1b20d6795016af73c22543
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-07-05 00:13:20 -0300

    Fix repository Produtos.

--- src/main/java/com/lopes/vendas/repository/Produtos.java
+++ src/main/java/com/lopes/vendas/repository/Produtos.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lopes.vendas.model.Produto;
 
-public interface ProdutosRepository extends JpaRepository<Produto, Long> {
+public interface Produtos extends JpaRepository<Produto, Long> {
 
 }
