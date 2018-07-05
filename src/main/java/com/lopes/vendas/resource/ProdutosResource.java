00971becab4dd159bffc3f34d0ef91a9e4154eb6
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-07-05 00:15:29 -0300

    Includes resource produtos.

---
+++ src/main/java/com/lopes/vendas/resource/ProdutosResource.java
@@ -0,0 +1,24 @@
+package com.lopes.vendas.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lopes.vendas.model.Produto;
+import com.lopes.vendas.repository.Produtos;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutosResource {
+
+	@Autowired
+	private Produtos produtos;
+
+	@GetMapping
+	public List<Produto> listar() {
+		return produtos.findAll();
+	}
+}
